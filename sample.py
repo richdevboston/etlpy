ab82710015e8da32bf41cf0e9d18119e2069561a
ferventdesert <buptzym@qq.com>
2016-03-24 13:04:34 +0800

    修改示例工程

--- sample.py
+++ sample.py
@@ -2,8 +2,10 @@
 import etl;
 
 
-etl.LoadProject('D:\我的工程.xml');
+etl.LoadProject('project.xml');
 tool=etl.modules['数据清洗ETL-链家二手房'];
+#tool=etl.modules['大众点评门店'];
+#etlcount is used for debug
 datas = tool.RefreshDatas(etlCount=100)
 i = 0;
 for r in datas:
