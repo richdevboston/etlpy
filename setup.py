f64db18445c289f81a3f6495df67e52897994938
desert.zym <desert.zym@alipay.com>
2017-05-04 14:48:19 +0800

    asd

--- setup.py
+++ setup.py
@@ -3,6 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name = "etlpy",
     version = "0.1",
-    packages=find_packages('src'),  # 包含所有src中的包
+    packages=['src'],  # 包含所有src中的包
 
 )
